858a8c229b0434f99afe918c14ba980cc788c968
Théophile Lemaitre <theophile.lemaitre@gmail.com>
2015-12-02 10:52:27 +0100

    created two models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202090908) do
+ActiveRecord::Schema.define(version: 20151202095057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,13 +36,20 @@ ActiveRecord::Schema.define(version: 20151202090908) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "reduction"
     t.text     "description"
     t.float    "pricettc"
     t.float    "priceclub"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "mail"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
